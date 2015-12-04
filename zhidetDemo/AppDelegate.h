d7939246c35a2d222d362adf51ef2a66a966ad1f
刘璐璐 <liululu@liululudembp.lan>
2015-12-04 11:50:45 +0800

    Initial Commit

---
+++ zhidetDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhidetDemo
+//
+//  Created by 刘璐璐 on 15/12/4.
+//  Copyright © 2015年 刘璐璐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhidetDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  zhidetDemo
+//
+//  Created by 刘璐璐 on 15/12/4.
+//  Copyright © 2015年 刘璐璐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
